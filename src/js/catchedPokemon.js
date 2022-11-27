3021be530d07b860ea3f91194cf6d0da24e04219
DanielVierh <daniel.vierheilig89@yahoo.de>
2022-11-27 23:05:11 +0100

    Added add new team members

--- src/js/catchedPokemon.js
+++ src/js/catchedPokemon.js
@@ -2,6 +2,8 @@ let myCatchedPokemons = [];
 let myTeam = [];
 
 const catchedPokemonContaier = document.getElementById("catchedPokemonContaier");
+const teamPokemonContainer = document.getElementById("teamPokemonContainer");
+
 
 let save_Object = {
     today_Date: '',
@@ -23,7 +25,7 @@ window.onload = init();
 
 
 function init() {
-    if(document.getElementById("catchPokeTag")) {
+    if (document.getElementById("catchPokeTag")) {
         load_SaveObj();
     }
 }
@@ -37,6 +39,7 @@ function load_SaveObj() {
 
         try {
             renderCatchedPokemons();
+            renderTeam();
         } catch (error) {
 
         }
@@ -49,10 +52,9 @@ function save_SaveObj() {
 }
 
 
+
 function renderCatchedPokemons() {
-    console.log(myCatchedPokemons);
-    for(let i = 0; i < myCatchedPokemons.length; i++) {
-        console.log(myCatchedPokemons[i].spriteFront);
+    for (let i = 0; i < myCatchedPokemons.length; i++) {
         let pokeCont = document.createElement('div');
         pokeCont.classList.add("pokemonContaier")
 
@@ -76,16 +78,104 @@ function renderCatchedPokemons() {
         let type = document.createElement("p");
         type.innerHTML = `Typ: ${myCatchedPokemons[i].type}`;
 
+        let number = document.createElement("p");
+        number.innerHTML = `Nr. ${myCatchedPokemons[i].id}`;
+
         infocont.appendChild(level)
         infocont.appendChild(type)
+        infocont.appendChild(number)
 
         pokeCont.appendChild(addBtn)
         pokeCont.appendChild(pokeimage)
         pokeCont.appendChild(pokename)
         pokeCont.appendChild(infocont)
 
-
-
         catchedPokemonContaier.appendChild(pokeCont)
+    }
+}
+
+
+
+// Pokemon hinzufügen
+const addBtn = document.querySelectorAll('.addButton');
+if (addBtn) {
+    addBtn.forEach((button) => {
+        button.addEventListener('click', () => {
+            let pokemonIndex = -1;
+            let addedPokemon;
+            for (let i = 0; i < myCatchedPokemons.length; i++) {
+                if (button.id === myCatchedPokemons[i].unique_ID) {
+                    pokemonIndex = i;
+                    break;
+                }
+            }
+
+            // Prüfen ob Pokemon gefunden
+            if (pokemonIndex >= 0) {
+                addedPokemon = myCatchedPokemons[pokemonIndex]
+                // console.log('addedPokemon', addedPokemon);
+
+                // check ob teamplatz frei
+                const freeTeamNumbers = myTeam.length;
+                if (freeTeamNumbers < 4) {
+                    //! check, ob dieses Pokemon schon im Team
+                    myTeam.push(addedPokemon);
+                    console.log(myTeam);
+                    renderTeam()
+                    save_SaveObj();
+                }else {
+                    alert("Kein Platz mehr frei")
+                }
+            }
+
+
+
+
+
+        });
+    });
+}
+
+
+
+function renderTeam() {
+    teamPokemonContainer.innerHTML = "";
+    for (let i = 0; i < myTeam.length; i++) {
+        let pokeCont = document.createElement('div');
+        pokeCont.classList.add("pokemonContaier")
+
+        let addBtn = document.createElement("div");
+        addBtn.innerHTML = "-"
+        addBtn.classList.add("deleteButton")
+        addBtn.id = myTeam[i].unique_ID
+
+        let pokeimage = document.createElement("img");
+        pokeimage.src = myTeam[i].spriteFront;
+
+        let pokename = document.createElement("p");
+        pokename.innerHTML = myTeam[i].name;
+
+        let infocont = document.createElement("div");
+        infocont.classList.add("infobox");
+
+        let level = document.createElement("p");
+        level.innerHTML = `Lv.${myTeam[i].level}`;
+
+        let type = document.createElement("p");
+        type.innerHTML = `Typ: ${myTeam[i].type}`;
+
+        let number = document.createElement("p");
+        number.innerHTML = `Nr. ${myTeam[i].id}`;
+
+        infocont.appendChild(level)
+        infocont.appendChild(type)
+        infocont.appendChild(number)
+
+        pokeCont.appendChild(addBtn)
+        pokeCont.appendChild(pokeimage)
+        pokeCont.appendChild(pokename)
+        pokeCont.appendChild(infocont)
+
+        teamPokemonContainer.appendChild(pokeCont)
     }
 }
