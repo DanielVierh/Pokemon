b4918ec05dd6c749a2ac5cd30cdf81842a62d271
DanielVierh <DanielVierh@users.noreply.github.com>
2023-10-08 14:11:31 +0200

    change text if no effect

--- src/js/battle.js
+++ src/js/battle.js
@@ -1007,7 +1007,7 @@ function animateProgressBar(damage, whoIsAffected, healVal, is_strike) {
                             atackerPokemon.name,
                         )} führt "${makeFirstLetterBig(
                             pokeMove.name,
-                        )}" aus. Die Attacke hat keine Wirkung.`,
+                        )}" aus. Die Attacke hat keine Wirkung auf ${currentWildPokemon.name}.`,
                     );
                 }else if(damage === 0 && is_strike === false) {
                     showInfoBox(
